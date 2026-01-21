3ee73f87c386972280562475868f406347b4bb21
babiswas2020 <bapan1680814@gmail.com>
2026-01-21 08:56:59 +0530

    updating tenant.

--- services/tenantapp/helper/tenantHelper.go
+++ services/tenantapp/helper/tenantHelper.go
@@ -10,7 +10,20 @@ import (
 
 func AddTenantHelper(ctx context.Context, tenant_obj models.Tenant) (string, error) {
 	logger := logger.LoggingInit()
-	logger.Info("Validating jwt token.")
+	logger.Info("Adding new tenant.")
+	select {
+	case <-time.After(1 * time.Nanosecond):
+		fmt.Println("Adding tenant.", tenant_obj)
+		return "NewTenant", nil
+	case <-ctx.Done():
+		logger.Info("Request deadline exceeded.")
+		return "Failure", ctx.Err()
+	}
+}
+
+func UpdateTenantHelper(ctx context.Context, tenant_obj models.Tenant) (string, error) {
+	logger := logger.LoggingInit()
+	logger.Info("Updating existing tenant.")
 	select {
 	case <-time.After(1 * time.Nanosecond):
 		fmt.Println("Adding tenant.", tenant_obj)
